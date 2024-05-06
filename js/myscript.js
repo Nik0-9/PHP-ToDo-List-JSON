e030b64dfe77b8ab821d540802d76945d6216b34
Nik0-9 <filannino.nicola9@gmail.com>
2024-05-06 16:42:23 +0200

    ADDED print new task in file data.json

--- js/myscript.js
+++ js/myscript.js
@@ -1,54 +1,63 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             carbonara: [],
             newAction: '',
-            apiUrl: 'server.php'
+            apiUrl: 'server.php',
+            lastId: null
         }
     },
-    methods:{
-        getData(){
-            axios.get(this.apiUrl).then((res)=>{
-                this.carbonara = res.data;
+    methods: {
+        getData() {
+            axios.get(this.apiUrl).then((res) => {
                 console.log(res.data);
+                this.carbonara = res.data;
+                
             });
         },
-        toggleLineThrough(id){
-            const item = this.carbonara.find((el)=>el.id === id);
-            console.log(item);
-            if(item){
-                item.done = !item.done
-            }
-        },
-        removeById(id){
-            const index = this.carbonara.findIndex((el)=>el.id === id);
-            if(index !== -1){
-                this.carbonara.splice(index, 1);
-            }
-        },
-        addAction(){
+        addAction() {
             const newObj = {
                 id: null,
                 text: this.newAction,
                 done: false
             }
             let newId = 0;
-            this.carbonara.forEach((el)=> {
-                if(newId < el.id){
-                    newId = el.id;
-                }
+
+            newObj.id = this.carbonara.length +1;
+            const data = new FormData();
+            data.append('id', newObj.id);
+            data.append('text', newObj.text);
+            data.append('done', newObj.done);
+            axios.post(this.apiUrl, data).then((res) => {
+                this.carbonara = res.data;
+                
             });
-            newObj.id = newId + 1;
-            this.carbonara.push(newObj);
+
             this.newAction = '';
             console.log(this.carbonara);
-        }
+        },
+
+        toggleLineThrough(id) {
+            const item = this.carbonara.find((el) => el.id === id);
+            console.log(item);
+            if (item) {
+                item.done = !item.done
+            }
+        },
+
+        removeById(id) {
+            const index = this.carbonara.findIndex((el) => el.id === id);
+            if (index !== -1) {
+                this.carbonara.splice(index, 1);
+            }
+        },
+
     },
-    
-    mounted(){
-        //console.log(this.carbonara);
+
+    created() {
         this.getData();
+        console.log(this.carbonara);
     }
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,6 +1,25 @@
 <?php
+
 $todoJson = file_get_contents("js/data.json");
 
-
+$method = $_SERVER['REQUEST_METHOD'];
+if($method !== 'GET') {
+    if($method === 'POST'){
+        //converto il json in un array php
+        $toDo = json_decode($todoJson, true);
+        if(isset($_POST['id'])) {
+            //creo un nuovo array associativo per il nuovo toDo
+            $task = [
+                'id' => (int) $_POST['id'],
+                'text'=> $_POST['text'],
+                'done'=> !(bool)$_POST['done'],
+            ];
+            //aggiungo il nuovo toDo all'array dei toDo
+            $toDo[] = $task;
+        }
+    }
+    $todoJson = json_encode($toDo, JSON_PRETTY_PRINT);
+    file_put_contents('js/data.json', $todoJson);
+}
 header("Content-Type: application/json");
 echo $todoJson;
